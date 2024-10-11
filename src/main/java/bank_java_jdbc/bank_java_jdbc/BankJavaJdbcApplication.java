a97c505ce0b7817d7ef16ebc2f427d57d6807cf6
Cauã <78801327+TeJoT4@users.noreply.github.com>
2024-10-11 08:20:32 -0300

    subindo projeto padrao

---
+++ src/main/java/bank_java_jdbc/bank_java_jdbc/BankJavaJdbcApplication.java
@@ -0,0 +1,13 @@
+package bank_java_jdbc.bank_java_jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankJavaJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankJavaJdbcApplication.class, args);
+	}
+
+}
